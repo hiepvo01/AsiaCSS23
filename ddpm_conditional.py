e958b87dfb70d18752ca1ccb6a8f8624ce4b0a82
hiepvo01 <vohi0311@gmail.com>
2022-12-06 16:44:29 +1100

    try

--- ddpm_conditional.py
+++ ddpm_conditional.py
@@ -161,7 +161,7 @@ def launch():
     args.dataset_path = r"./data/cifar-10-batches-py/"
     args.device = "cuda"
     args.lr = 3e-4
-    args.num_clients = 8
+    args.num_clients = 10
     
     train(args)
 
